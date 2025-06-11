9fab60daf75056e8073677fd0442f3d0c54c61f9
Tóth Dávid <45020602+tDavid93@users.noreply.github.com>
2025-06-11 09:46:17 +0200

    Fix truncation bugs in backend files

--- backend/app/mtgjson_db/users.py
+++ backend/app/mtgjson_db/users.py
@@ -1,21 +1,13 @@
 from mtgjson_db.database import Base
-
-
-#Fields named "type" are reserved in Python, so we use "card_type" instead
-from sqlalchemy import Column, Integer, Float, String, Boolean, Date
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
-from sqlalchemy import ForeignKey
-
-
-import passlib.hash as _hash
-
 
 class User(Base):
     __tablename__ = 'user'
-    
-    id = Column(Integer,primary_key=True,autoincrement=True)
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
     username = Column(String(255))
     password = Column(String(255))
-    
-    
+
+    decks = relationship("Decks", back_populates="user")
+
